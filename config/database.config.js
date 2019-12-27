125d04f7901a2ba162e578564b83cba064e3655c
Andrés López Albín <andres.albin93@gmail.com>
2019-12-27 16:03:54 +0100

    added travis and docker files

--- config/database.config.js
+++ config/database.config.js
@@ -1,7 +1,6 @@
-require('dotenv').config();
 
 module.exports = {
     //We take MongoDB url and server's port from .env file
-    url: process.env.DB_URL,
-    port: process.env.PORT
+    url: process.env.DB_URL || "mongodb://localhost:27017/auth", 
+    port: process.env.PORT || 3000,
 }
