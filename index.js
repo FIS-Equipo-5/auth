925e0bb3752d381af229666094b4f8a57a392979
Andrés López Albín <andres.albin93@gmail.com>
2019-12-30 13:21:44 +0100

    added async await test

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const dbConfig = require('./config/database.config.js');
 console.log("Starting API server at "+dbConfig.port);
 
 dbConnect().then(
-    () => {
+    async () => {
         app.listen(dbConfig.port);
         console.log("Server is listening on port " + dbConfig.port);
         const User = require('./app/models/user.model');
         console.log("Saving User");
         user = new User({ name: 'user', email: 'user@email.com', password: 'password' });
-        let response = user.save()
+        let response = await user.save()
         console.log("Added user " + response);
     },
     err => {
