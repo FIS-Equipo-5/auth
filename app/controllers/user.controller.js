6551046ea392fc6670f1983f91c32bed17cd5d70
Andrés López Albín <andres.albin93@gmail.com>
2019-12-30 13:14:00 +0100

    added dummy user and loggers

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -4,13 +4,16 @@ const jwt = require('jsonwebtoken');
 module.exports = {
  create: function(req, res, next) {
   
-  userModel.create({ name: req.body.name, email: req.body.email, password: req.body.password }, function (err, result) {
-      if (err) 
-       next(err);
-      else
-       res.json({status: "success", message: "User added successfully!!!", data: null});
-      
-    });
+      userModel.create({ name: req.body.name, email: req.body.email, password: req.body.password }, function (err, result) {
+         console.log('create method!');
+         if (err) {
+            console.log('ERROR CREATE:'+ error);
+            next(err);
+         }
+         else {
+            res.json({ status: "success", message: "User added successfully!!!", data: null });
+         }
+      });
  },
 authenticate: function(req, res, next) {
   userModel.findOne({email:req.body.email}, function(err, userInfo){
--- index.js
+++ index.js
@@ -9,6 +9,11 @@ dbConnect().then(
     () => {
         app.listen(dbConfig.port);
         console.log("Server is listening on port " + dbConfig.port);
+        const User = require('./app/models/user.model');
+        console.log("Saving User");
+        user = new User({ name: 'user', email: 'user@email.com', password: 'password' });
+        let response = user.save()
+        console.log("Added user " + response);
     },
     err => {
         console.log("Connection error: "+err);
