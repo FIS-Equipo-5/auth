e83b9b4800df2f794679681a4de011c6b1536d71
Jose Manuel Luque Mendoza <jmlmdev@gmail.com>
2019-12-30 21:12:30 +0100

    Introduccion de console.logs para probar

--- index.js
+++ index.js
@@ -31,10 +31,11 @@ dbConnect().then(
         console.log("BD is located in " + dbConfig.url + ' ' + dbConfig.port);
         app.listen(dbConfig.port, async () => {
             console.log("Saving User");
-            // const User = require('./app/models/user.model');
-            // user = new User({ name: 'user', email: 'user@email.com', password: 'password' });
-            // let response = await user.save()
-            // console.log("Added user " + response);
+             const User = require('./app/models/user.model');
+             user = new User({ name: 'user', email: 'user@email.com', password: 'password' });
+            console.log("User const: ", user);
+            let response = await user.save()
+            console.log("Added user " + response);
         });
         console.log("Server is listening on " + dbConfig.port);
     },
